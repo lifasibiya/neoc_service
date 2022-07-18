aa999ffdd871a0077646c019195fca762719f206
lifasibiya <sibiyalifa@gmail.com>
2022-07-18 16:57:14 +0200

    configure sql DBContext

---
+++ neoc/Data/DBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace neoc.Data
+{
+    public class NEOCDBContext: DbContext
+    {
+        public NEOCDBContext(DbContextOptions<NEOCDBContext> options):base(options)
+        {
+
+        }
+    }
+}
--- neoc/Program.cs
+++ neoc/Program.cs
@@ -1,3 +1,7 @@
+using System.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using neoc.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var conn = builder.Configuration["ConnectionStrings:neoc"];
+builder.Services.AddDbContext<NEOCDBContext>(cfg => cfg.UseSqlServer(conn));
 
 var app = builder.Build();
 
